afd21fb30f5a8a830cc704245f218130a30f0a6d
Praveen Renganathan <praveenmanickam.renganathan@kellogg.northwestern.edu>
2024-03-09 17:08:44 +0000

    Update1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
+    #@user["first_name"] = params["first_name"]
+    #@user["last_name"] = params["last_name"]
     @user["email"] = params["email"]
     @user["password"] = params["password"]
     @user.save
